cbb2cec84ad7b3a132c773730c0ca27a25dea011
FFGFlash <26831072+FFGFlash@users.noreply.github.com>
2022-10-11 18:54:45 -0500

    Update startup.lua

--- startup.lua
+++ startup.lua
@@ -34,38 +34,39 @@ function Draw()
   term.setBackgroundColor(colors.lightGray)
   term.clear()
   term.setCursorPos(1,1)
-  if #ReactorIds == 0 then
-    term.write("No Reactors Online...")
-    return
-  elseif Reactors == {} then
-    term.write("Awaiting Reactor Data...")
-    return
-  elseif not SelectedReactor then
-    term.write("Awaiting Reactor Data...")
-    return
-  end
-
-  local function writeCentered(text)
-    local len = string.len(text)
-    local x, y = term.getCursorPos()
-    term.setCursorPos(x - len / 2, y)
-    term.write(text)
-  end
-
-  term.setBackgroundColor(colors.lightBlue)
-  term.setTextColor(colors.black)
-  term.clearLine()
-  term.setCursorPos(width / 2, 1)
-  writeCentered(SelectedReactor.Name)
-  term.setBackgroundColor(colors.black)
-  term.setTextColor(colors.white)
-  term.setCursorPos(2,3)
-  print("Control Rods: "..SelectedReactor.NumberOfControlRods)
-  print("Energy Produced: "..SelectedReactor.Energy.ProducedLastTick)
-  print("Energy (%): "..(math.floor(SelectedReactor.Energy.Stored / SelectedReactor.Energy.Capacity * 100)).."%")
-  print("Fuel Consumed: "..SelectedReactor.Fuel.ConsumedLastTick)
-  print("Fuel (%): "..(math.floor(SelectedReactor.Fuel.Amount / SelectedReactor.Fuel.Capacity * 100)).."%")
-  print("Waste (%): "..(math.floor(SelectedReactor.Fuel.Waste / SelectedReactor.Fuel.Capacity * 100)).."%")
+  print(Reactors)
+  -- if #ReactorIds == 0 then
+  --   term.write("No Reactors Online...")
+  --   return
+  -- elseif Reactors == {} then
+  --   term.write("Awaiting Reactor Data...")
+  --   return
+  -- elseif not SelectedReactor then
+  --   term.write("Awaiting Reactor Data...")
+  --   return
+  -- end
+  --
+  -- local function writeCentered(text)
+  --   local len = string.len(text)
+  --   local x, y = term.getCursorPos()
+  --   term.setCursorPos(x - len / 2, y)
+  --   term.write(text)
+  -- end
+  --
+  -- term.setBackgroundColor(colors.lightBlue)
+  -- term.setTextColor(colors.black)
+  -- term.clearLine()
+  -- term.setCursorPos(width / 2, 1)
+  -- writeCentered(SelectedReactor.Name)
+  -- term.setBackgroundColor(colors.black)
+  -- term.setTextColor(colors.white)
+  -- term.setCursorPos(2,3)
+  -- print("Control Rods: "..SelectedReactor.NumberOfControlRods)
+  -- print("Energy Produced: "..SelectedReactor.Energy.ProducedLastTick)
+  -- print("Energy (%): "..(math.floor(SelectedReactor.Energy.Stored / SelectedReactor.Energy.Capacity * 100)).."%")
+  -- print("Fuel Consumed: "..SelectedReactor.Fuel.ConsumedLastTick)
+  -- print("Fuel (%): "..(math.floor(SelectedReactor.Fuel.Amount / SelectedReactor.Fuel.Capacity * 100)).."%")
+  -- print("Waste (%): "..(math.floor(SelectedReactor.Fuel.Waste / SelectedReactor.Fuel.Capacity * 100)).."%")
 end
 
 while Running do
