1fe869306af362609a19ef62a7e9d999e5f9dd1c
FFGFlash <26831072+FFGFlash@users.noreply.github.com>
2022-10-11 19:03:55 -0500

    Update startup.lua

--- startup.lua
+++ startup.lua
@@ -9,6 +9,7 @@ local Reactors = {}
 local Running = true
 local Reactor = 1
 local SelectedReactor = {}
+local next = next
 
 Events:connect("terminate", function() Running = false end)
 Events:connect("rednet_message", function(sender, req, protocol)
@@ -34,39 +35,38 @@ function Draw()
   term.setBackgroundColor(colors.lightGray)
   term.clear()
   term.setCursorPos(1,1)
-  print(Reactors)
-  -- if #ReactorIds == 0 then
-  --   term.write("No Reactors Online...")
-  --   return
-  -- elseif Reactors == {} then
-  --   term.write("Awaiting Reactor Data...")
-  --   return
-  -- elseif not SelectedReactor then
-  --   term.write("Awaiting Reactor Data...")
-  --   return
-  -- end
-  --
-  -- local function writeCentered(text)
-  --   local len = string.len(text)
-  --   local x, y = term.getCursorPos()
-  --   term.setCursorPos(x - len / 2, y)
-  --   term.write(text)
-  -- end
-  --
-  -- term.setBackgroundColor(colors.lightBlue)
-  -- term.setTextColor(colors.black)
-  -- term.clearLine()
-  -- term.setCursorPos(width / 2, 1)
-  -- writeCentered(SelectedReactor.Name)
-  -- term.setBackgroundColor(colors.black)
-  -- term.setTextColor(colors.white)
-  -- term.setCursorPos(2,3)
-  -- print("Control Rods: "..SelectedReactor.NumberOfControlRods)
-  -- print("Energy Produced: "..SelectedReactor.Energy.ProducedLastTick)
-  -- print("Energy (%): "..(math.floor(SelectedReactor.Energy.Stored / SelectedReactor.Energy.Capacity * 100)).."%")
-  -- print("Fuel Consumed: "..SelectedReactor.Fuel.ConsumedLastTick)
-  -- print("Fuel (%): "..(math.floor(SelectedReactor.Fuel.Amount / SelectedReactor.Fuel.Capacity * 100)).."%")
-  -- print("Waste (%): "..(math.floor(SelectedReactor.Fuel.Waste / SelectedReactor.Fuel.Capacity * 100)).."%")
+  if #ReactorIds == 0 then
+    term.write("No Reactors Online...")
+    return
+  elseif next(Reactors) == nil then
+    term.write("Awaiting Reactor Data...")
+    return
+  elseif next(SelectedReactor) == nil then
+    term.write("Awaiting Reactor Data...")
+    return
+  end
+
+  local function writeCentered(text)
+    local len = string.len(text)
+    local x, y = term.getCursorPos()
+    term.setCursorPos(x - len / 2, y)
+    term.write(text)
+  end
+
+  term.setBackgroundColor(colors.lightBlue)
+  term.setTextColor(colors.black)
+  term.clearLine()
+  term.setCursorPos(width / 2, 1)
+  writeCentered(SelectedReactor.Name)
+  term.setBackgroundColor(colors.black)
+  term.setTextColor(colors.white)
+  term.setCursorPos(2,3)
+  print("Control Rods: "..SelectedReactor.NumberOfControlRods)
+  print("Energy Produced: "..SelectedReactor.Energy.ProducedLastTick)
+  print("Energy (%): "..(math.floor(SelectedReactor.Energy.Stored / SelectedReactor.Energy.Capacity * 100)).."%")
+  print("Fuel Consumed: "..SelectedReactor.Fuel.ConsumedLastTick)
+  print("Fuel (%): "..(math.floor(SelectedReactor.Fuel.Amount / SelectedReactor.Fuel.Capacity * 100)).."%")
+  print("Waste (%): "..(math.floor(SelectedReactor.Fuel.Waste / SelectedReactor.Fuel.Capacity * 100)).."%")
 end
 
 while Running do
